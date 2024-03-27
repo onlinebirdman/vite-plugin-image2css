617095896ea47d6ea6d1a72e1f5fd9f31d52ab85
jp <137669276@qq.com>
2024-03-27 11:33:44 +0800

    fix add cjs build

--- build.config.ts
+++ build.config.ts
@@ -1,15 +1,18 @@
-import { defineBuildConfig } from "unbuild";
+import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   // If entries is not provided, will be automatically inferred from package.json
   entries: [
-    "./src/index.ts",
+    './src/index.ts'
   ],
 
   // Change outDir, default is 'dist'
-  outDir: "dist",
+  outDir: 'dist',
 
   clean: true,
   // Generates .d.ts declaration file
   declaration: true,
-});
+  rollup: {
+    emitCJS: true
+  }
+})
